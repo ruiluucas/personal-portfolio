a2ad7eca4ec796e8a6214b6b92e222f4cf42b17a
Rui Gomes <lucasantony815@gmail.com>
2023-05-09 23:20:05 -0300

    add physics

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,12 @@
-import { useState, Children, useRef } from 'react'
+import { useState, Children, useRef, Suspense } from 'react'
 import * as THREE from 'three'
-import { useTrail, a } from '@react-spring/web'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { useTrail, animated, useSpring } from '@react-spring/web'
+import { a } from "@react-spring/three"
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { Box, ContactShadows, Environment, Float, Html, MeshReflectorMaterial, OrbitControls, Sky, Stars, Trail, useGLTF } from "@react-three/drei"
 import { Bloom, EffectComposer } from '@react-three/postprocessing'
 import MatrixScreen from './MatrixScreen'
+import { Physics, useSphere } from '@react-three/cannon'
 
 const TrailText = ({ open, children }) => {
   const items = Children.toArray(children)
@@ -18,9 +20,9 @@ const TrailText = ({ open, children }) => {
   return (
     <div>
       {trail.map(({ height, ...style }, index) => (
-        <a.div key={index} className="text-black text-7xl md:text-8xl tracking-tight font-extrabold leading-10 h-14 md:h-20" style={style}>
-          <a.div style={{ height }}>{items[index]}</a.div>
-        </a.div>
+        <animated.div key={index} className="text-black text-7xl md:text-8xl tracking-tight font-extrabold leading-10 h-14 md:h-20" style={style}>
+          <animated.div style={{ height }}>{items[index]}</animated.div>
+        </animated.div>
       ))}
     </div>
   )
@@ -45,6 +47,7 @@ function NotebookContent() {
   return (
       <group position={[0, -1.5, -1.8]} rotation={[0.3, -0.3, 0]}>
         <group rotation-x={-0.425} position={[0, -0.04, 0.41]}>
+        <Suspense fallback={null}>
           <group position={[0, 2.96, -0.13]} rotation={[Math.PI / 2, 0, 0]}>
             <mesh material={materials.aluminium} geometry={nodes['Cube008'].geometry} />
             <mesh material={materials['matte.001']} geometry={nodes['Cube008_1'].geometry} />
@@ -56,6 +59,7 @@ function NotebookContent() {
               </Html>
             </mesh>
           </group>
+          </Suspense>
         </group>
         <mesh material={materials.keys} geometry={nodes.keyboard.geometry} position={[1.79, 0, 3.45]} />
         <group position={[0, -0.1, 3.39]}>
@@ -68,6 +72,9 @@ function NotebookContent() {
 }
 
 export default function Main() {
+  const [ref, api] = useSphere(() => ({ mass: 1, args: [0.5], position: [0, 5, 0] }))
+
+  
   return (
     <main className='pb-60 bg-black flex-1'>
       <div className="h-full">
@@ -78,11 +85,13 @@ export default function Main() {
           <pointLight position={[10, 10, 10]} intensity={1.5} />
           <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
           <Environment preset="city" />
-          <Float speed={1} rotationIntensity={1} floatIntensity={5}>
-            <NotebookContent />
-            <Electron position={[0, 0, 0.5]} rotation={[3.14 / 1.5 - 0.2, 0, 0]} speed={1.8} />
-            <Electron position={[0, 0, 0.5]} rotation={[0, 2, -3]} speed={2} />
-            <Electron position={[0, 0, 0.5]} rotation={[0, 0, 0]} speed={1.6} />
+          <Float speed={1} rotationIntensity={1} floatIntensity={5} position={[5, 0, 0]}>
+            <Physics>
+              <NotebookContent />
+              <Electron position={[0, 0, 0.5]} rotation={[3.14 / 1.5 - 0.2, 0, 0]} speed={1.8} />
+              <Electron position={[0, 0, 0.5]} rotation={[0, 2, -3]} speed={2} />
+              <Electron position={[0, 0, 0.5]} rotation={[0, 0, 0]} speed={1.6} /> 
+            </Physics> 
           </Float>
           <OrbitControls />
           <Stars saturation={0} count={400} speed={0.5} />
@@ -95,21 +104,47 @@ export default function Main() {
   )
 }
 
-function Electron({ radius = 6.5, speed = 0.5, ...props }) {
-  const ref = useRef()
+function Electron({ radius = 6.5, speed = 0.1, ...props }) {
+  const ball = useRef()
+  const [spring, setSpring] = useSpring(() => ({
+    position: [-50, -10, -10],
+    config: {
+      mass: 5,
+      friction: 10,
+    }
+  }))
+
   useFrame((state) => {
     const t = state.clock.getElapsedTime() * speed
-    ref.current.position.set(Math.sin(t) * radius, (Math.cos(t) * radius * Math.atan(t)) / Math.PI / 0.5, 0)
+
+    if(ball.current.position.y < -15) {
+      setTimeout(() => {
+        setSpring({
+          position: [0, -10, -10]
+        })
+      }, 5000)
+    } else {
+      setSpring({
+        position: [Math.sin(t) * radius, (Math.cos(t) * radius * Math.atan(t)) / Math.PI / 0.5, 0]
+      })
+    }
   })
+
   return (
     <group {...props}>
-      <Trail width={5} length={3} color={new THREE.Color(0, 10, 0)} attenuation={(t) => t * t}>
-        <mesh ref={ref}>
-          <sphereGeometry args={[0.25]}  />
+      <Trail width={5} length={5} color={new THREE.Color(0, 1, 0)} attenuation={(t) => t * t}>
+        <a.mesh ref={ball} position={spring.position}>
+          <sphereGeometry args={[0.25]} />
           <meshBasicMaterial color={[0, 10, 0]} toneMapped={false} />
-        </mesh>
+        </a.mesh>
       </Trail>
     </group>
   )
 }
+
+
+
+
+
+
 
