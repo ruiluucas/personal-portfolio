f3fb05d5f6c5d5783c1222f2075e37e6d34673a3
Rui Gomes <lucasantony815@gmail.com>
2023-05-14 18:31:31 -0300

    add framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,14 @@ import Fingerspose from "./components/Fingerspose";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import React, { useState, CSSProperties, useEffect } from 'react'
-import { useTransition, animated, useSpringRef } from '@react-spring/web'
+import { useTransition, animated, useSpringRef, SpringRef } from '@react-spring/web'
+import { motion } from "framer-motion"
 
 export default function App() {
   return (
-    <header className="h-screen flex flex-col">
-      <A />
-      <Fingerspose />
-    </header>
-  )
-}
-
-const pages = [
-  ({ style }) => <animated.div style={{ ...style, background: 'lightpink' }}>A</animated.div>,
-  ({ style }) => <animated.div style={{ ...style, background: 'lightblue' }}>B</animated.div>,
-  ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}>C</animated.div>,
-]
-
-function A() {
-  const [index, set] = useState(0)
-  const onClick = () => set(state => (state + 1) % 3)
-  const transRef = useSpringRef()
-  const transitions = useTransition(index, {
-    ref: transRef,
-    keys: null,
-    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
-    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
-  })
-
-  useEffect(() => {
-    transRef.start()
-
-    setTimeout(() => {
-      onClick()
-    }, 3000)
-  }, [index])
-  return (
-    <div onClick={onClick}>
-      {transitions((style, i) => {
-        const Page = pages[i]
-        return <Page style={style} />
-      })}
+    <div className="h-screen flex flex-col">
+      <Header />
+      <Main />
     </div>
   )
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,14 +3,14 @@ import * as THREE from 'three'
 import { useTrail, animated, useSpring, easings, useSpringRef } from '@react-spring/web'
 import Space from '../3D/Space'
 import { useSelector } from 'react-redux'
+import { motion } from "framer-motion"
 
 const TrailText = ({ children }) => {
   const items = Children.toArray(children)
-  const activeCanvas = useSelector((state) => state.style.activeCanvas)
   const trail = useTrail(items.length, {
     config: { mass: 7, tension: 300, friction: 100 },
-    from: { opacity: 0, x: 80, height: activeCanvas ? 0 : 110 },
-    to: { opacity: !activeCanvas ? 1 : 0, x: !activeCanvas ? 20 : 80, height: !activeCanvas ? 110 : 0 },
+    from: { opacity: 0, x: 80, height: 110 },
+    to: { opacity: 1 , x: 20, height: 0 },
   });
     
   return (
@@ -26,11 +26,37 @@ const TrailText = ({ children }) => {
 
 function TextMain() {
   return (
-    <div className="flex items-center justify-center md:justify-start md:m-40 md:mt-0 sm:m-0 sm:justify-center h-full">
-      <TrailText>
-        <span className='text-white'>Let's</span>
-        <span className='text-white'>Program!</span>
-      </TrailText>
+    <div className="flex flex-col items-start justify-center md:justify-center md:m-40 md:mt-0 sm:m-0 sm:justify-center h-full">
+      <motion.div
+        initial={{ opacity: 0, x: 80 }} 
+        animate={{ opacity: 1 , x: 20 }} 
+        transition={{ duration: 1.5 }} 
+        className="text-black text-7xl md:text-8xl tracking-tight font-extrabold leading-10 h-14 md:h-20"
+      >
+        <motion.span 
+          initial={{ height: 110 }} 
+          animate={{ height: 0 }} 
+          transition={{ duration: 1.5 }} 
+          className='text-white'
+        >
+          Let's
+        </motion.span>
+      </motion.div>
+      <motion.div 
+        initial={{ opacity: 0, x: 80 }} 
+        animate={{ opacity: 1 , x: 20 }} 
+        transition={{ duration: 2 }} 
+        className="text-black text-7xl md:text-8xl tracking-tight font-extrabold leading-10 h-14 md:h-20"
+      >
+        <motion.span 
+        initial={{ height: 110 }} 
+        animate={{ height: 0 }} 
+        transition={{ duration: 3 }} 
+        className='text-white'
+        >
+          Program!
+        </motion.span>
+      </motion.div>
     </div>
   )
 }
@@ -41,11 +67,7 @@ export default function Main() {
   return (
     <main className='pb-60 bg-black flex-1'>
       <div className="h-full z-10"><TextMain /></div>
-      { 
-        activeCanvas ? 
-        <div style={{ zIndex: 1 }} className="absolute h-screen w-full top-0"><Space /></div> :
-        <div></div>
-      }
+      { activeCanvas && <div style={{ zIndex: 1 }} className="absolute h-screen w-full top-0"><Space /></div> }
     </main>
   )
 }
