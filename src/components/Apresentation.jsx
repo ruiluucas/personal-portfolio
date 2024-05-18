7fceaf939ecebecf9ca6a6b6c5443e1f1d2fe91d
Rui Gomes <lucasantony815@gmail.com>
2024-05-18 03:13:51 -0300

    responsive 2x

--- src/components/Apresentation.jsx
+++ src/components/Apresentation.jsx
@@ -8,20 +8,54 @@ export default function Apresentation() {
         <div className="absolute z-0 h-full w-full">
             <Space />
         </div>
-        <div className="absolute z-20 flex h-full w-full flex-col text-white">
-            <main style={{ fontFamily: '"Instrument Serif", sans-serif' }} className="flex flex-1 items-center font-extrabold">
-                <div className="md:m-12 m-4 flex flex-col">
-                    <div className="flex flex-col">
+        <AnimatePresence>
+            <div 
+            style={{ fontFamily: '"Instrument Serif", sans-serif' }} 
+            className="absolute z-30 flex h-full w-full text-white">
+                <div className="flex justify-center items-end sm:items-center">
+                    <div className="flex flex-col leading-3 m-10 mb-20 sm:m-20">
+                        <MotionConfig transition={{ duration: 1.6, ease: 'circOut' }}>
+                            <motion.p 
+                            key="name" 
+                            initial={{ opacity: 0, x: 80 }} 
+                            animate={{ opacity: 1, x: 0 }} 
+                            exit={{ opacity: 0 }} 
+                            className="tracking-tight sm:text-7xl text-4xl">
+                            @rui_luucas
+                            </motion.p>
+                        </MotionConfig>
+                        <MotionConfig transition={{ duration: 2, ease: 'circOut' }}>
+                            <motion.p 
+                            key="title" 
+                            initial={{ opacity: 0, x: 80 }} 
+                            animate={{ opacity: 1, x: 0, }} 
+                            exit={{ opacity: 0 }} 
+                            className="tracking-tight sm:text-7xl text-4xl">
+                            software developer
+                            </motion.p>
+                        </MotionConfig>
+                    </div>
+                </div>
+            </div>
+        </AnimatePresence>
+        </>
+    )
+}
+
+/*
+<main  className="flex flex-1 justify-center items-center font-extrabold">
+                <div className="md:m-12 m-4 flex flex-col  justify-center items-center">
+                    <div className="flex flex-col items-center">
                         <AnimatePresence>
-                        <div>
+                        <div className="flex items-center flex-col">
                             <MotionConfig transition={{ duration: 1.6, ease: 'circOut' }}>
                             <motion.p 
                                 key="name" 
                                 initial={{ opacity: 0, x: 80 }} 
                                 animate={{ opacity: 1, x: 0 }} 
                                 exit={{ opacity: 0 }} 
-                                className="md:text-7xl tracking-tight text-5xl">
-                                Rui Lucas
+                                className="md:text-8xl tracking-tight text-6xl">
+                                @rui_luucas
                             </motion.p>
                             </MotionConfig>
                             <MotionConfig transition={{ duration: 2, ease: 'circOut' }}>
@@ -31,7 +65,7 @@ export default function Apresentation() {
                                 animate={{ opacity: 1, x: 0, }} 
                                 exit={{ opacity: 0 }} 
                                 className="md:text-5xl tracking-tight text-5xl">
-                                Software Developer
+                                software developer
                             </motion.p>
                             </MotionConfig>
                         </div>
@@ -39,7 +73,4 @@ export default function Apresentation() {
                     </div>
                 </div>
             </main>
-        </div>
-        </>
-    )
-}
+*/
